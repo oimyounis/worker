63a496b250fb2fbe7a5351238e264bc4ed791eee
Omar Younis <thehaker_2005@hotmail.com>
2019-02-15 10:30:53 +0300

    init

---
+++ setup.sh
@@ -0,0 +1,2 @@
+pip install -r /src/worker/requirements.txt --trusted-host files.pythonhosted.org --trusted-host pypi.org --trusted-host python.pypi.org;
+python /src/worker/log_worker.py
---
+++ worker_config.py
@@ -0,0 +1,31 @@
+# Database Config
+PG_HOST = "127.0.0.1"
+PG_DB = "telemetry"
+PG_USER = "postgres"
+PG_PASSWORD = "postgres"
+PG_PORT = 5432
+
+
+# Broker Config
+BROKER_HOSTNAME = '127.0.0.1'
+BROKER_PORT = 1883
+
+
+# Client Config
+CLIENT_ID = 'mqtt_log_worker'
+CLEAN_SESSION = False
+KEEP_ALIVE_SECONDS = 65535
+USERNAME = None
+PASSWORD = None
+
+TELEMETRY = {'temp': 'temperature', 'bp': 'pressure'}
+
+
+# Client Event Handlers
+on_connect = None
+on_message = None
+on_publish = None
+on_subscribe = None
+on_unsubscribe = None
+on_disconnect = None
+
