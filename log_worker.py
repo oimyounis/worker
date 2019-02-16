1c8e75db465b96d1b380813fb5c6b760472734b1
Omar Younis <thehaker_2005@hotmail.com>
2019-02-16 15:58:53 +0300

    update

--- log_worker.py
+++ log_worker.py
@@ -1,116 +1,100 @@
+import time
+from datetime import datetime
 import json
-import psycopg2
+
 import paho.mqtt.client as mqtt
-from lib.utils import load_config, dblog
+
+# from models import TemperatureLog
+
+import psycopg2
+# from influxdb import InfluxDBClient
 
 
-config = load_config()
+TOPICS = {
+    'TEMPERATURE': 'sensors/temperature',
+    'PROXIMITY': 'sensors/proximity',
+}
 
-conn = None
-cur = None
-
-
-def pgconnect():
-    global conn, cur
-    if not conn or conn and conn.closed > 0:
-        conn = psycopg2.connect("host=%s dbname=%s user=%s password=%s port=%s" %
-                                (config['PG_HOST'], config['PG_DB'], config['PG_USER'], config['PG_PASSWORD'],
-                                 config['PG_PORT']))
-        cur = conn.cursor()
+# conn = psycopg2.connect("host=%s dbname=%s user=%s password=%s port=%s" % ("35.240.74.244", "metrics", "postgres", "postgres", 5432))
+# print(conn)
+# cur = conn.cursor()
+# print(cur)
+# influx = InfluxDBClient('192.168.42.248', 8086, 'root', 'root', 'test')
 
 
-if 'on_connect' in config and config['on_connect'] is not None:
-    from worker_config import on_connect
-else:
-    # The callback for when the client receives a CONNACK response from the server.
-    def on_connect(client, userdata, flags, rc):
-        pgconnect()
-        print("Connected with result code: " + str(rc))
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code " + str(rc))
 
-        # Subscribing in on_connect() means that if we lose the connection and
-        # reconnect then subscriptions will be renewed.
-        # client.subscribe('devices/#', qos=2)
-        client.subscribe('things/#', qos=2)
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe('things/#', qos=2)
 
 
-if 'on_message' in config and config['on_message'] is not None:
-    from worker_config import on_message
-else:
-    def on_message(client, userdata, msg):
-        pgconnect()
-        handle_message(msg)
+def on_disconnect(*args):
+    print('received disconnect signal')
+    if conn and cur:
+        print('will close connection to db')
+        cur.close()
+        conn.close()
+        print('closed connection to db')
 
 
-if 'on_publish' in config and config['on_publish'] is not None:
-    from worker_config import on_publish
-else:
-    def on_publish(): pass
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    print('in << ', msg.topic)
+    # handle_message(msg)
+    # print('in %s' % msg.payload)
 
 
-if 'on_subscribe' in config and config['on_subscribe'] is not None:
-    from worker_config import on_subscribe
-else:
-    def on_subscribe(): pass
-
-
-if 'on_unsubscribe' in config and config['on_unsubscribe'] is not None:
-    from worker_config import on_unsubscribe
-else:
-    def on_unsubscribe(): pass
-
-
-if 'on_disconnect' in config and config['on_disconnect'] is not None:
-    from worker_config import on_disconnect
-else:
-    def on_disconnect(client, userdata, rc):
-        print('received disconnect signal')
-        if conn and cur:
-            print('will close connection to db')
-            cur.close()
-            conn.close()
-            print('closed connection to db')
+def get_command_value(msg):
+    params = msg.split(',')
+    reading = params[0].split(':')
+    clientid = params[1].split(':')
+    return reading[1], clientid[1]
 
 
 def handle_message(msg):
-    print('in')
-    # payload = msg.payload.decode()
-    # segs = msg.topic.split('/')
+    payload = json.loads(msg.payload.decode())
+    # reading, clientid = get_command_value(msg.payload.decode())
+    print(payload)
 
-    # print(msg.topic, payload)
-    #
-    # if len(segs) == 4:
-    #     if segs[3].lower() in config['TELEMETRY']:
-    #         device_id = segs[1]
-    #         dblog(conn, cur, config['TELEMETRY'][segs[3]],
-    #               ('device_id', 'value'),
-    #               (device_id, payload))
+    if msg.topic == TOPICS['TEMPERATURE']:
+        print('will insert')
+        cur.execute('INSERT INTO conditions (time, sensor_id, temperature, humidity) VALUES (NOW(), %s, %s, %s)', (payload['sensor_id'], payload['temperature'], payload['humidity']))
+        conn.commit()
+        print('inserted')
+        # json_body = [
+        #     {
+        #         "measurement": "temperature",
+        #         "tags": {
+        #             "sensor_id": clientid,
+        #         },
+        #         "fields": {
+        #             "value": reading
+        #         }
+        #     }
+        # ]
+        # influx.write_points(json_body)
+    #     obj = TemperatureLog.create(value=value, created_at=datetime.now())
+        # print('LOG: logged temp in DB -> UUID: %s' % obj.id)
 
-    # payload = json.loads(msg.payload.decode())
-    # segs = msg.topic.split('/')
-    #
-    # print(msg.topic, msg.payload)
-    #
-    # if len(segs) == 3:
-    #     if segs[2].lower() in config['TELEMETRY']:
-    #         device_id = segs[1]
-    #         dblog(conn, cur, segs[2],
-    #               ('device_id', 'temperature', 'humidity', 'pressure'),
-    #               (device_id, payload['temperature'], payload['humidity'], payload['pressure']))
 
+BROKER_HOSTNAME = 'iot.slash-api.com'
+CLIENT_ID = 'mqtt_monitor_server'
 
-client = mqtt.Client(client_id=config['CLIENT_ID'], clean_session=config['CLEAN_SESSION'])
+client = mqtt.Client(client_id=CLIENT_ID, clean_session=True)
 client.on_connect = on_connect
 client.on_message = on_message
-client.on_publish = on_publish
-client.on_subscribe = on_subscribe
-client.on_unsubscribe = on_unsubscribe
 client.on_disconnect = on_disconnect
+# client.username_pw_set("admin", "public")
 
-if 'USERNAME' in config and config['USERNAME'] is not None and 'PASSWORD' in config and config['PASSWORD'] is not None:
-    client.username_pw_set(config['USERNAME'], config['PASSWORD'])
+client.connect(BROKER_HOSTNAME, 1883, 60)
 
-client.connect(config['BROKER_HOSTNAME'], config['BROKER_PORT'], config['KEEP_ALIVE_SECONDS'])
-
+# Blocking call that processes network traffic, dispatches callbacks and
+# handles reconnecting.
+# Other loop*() functions are available that give a threaded interface and a
+# manual interface.
 client.loop_forever()
 
 if conn and cur:
@@ -118,4 +102,3 @@ if conn and cur:
     cur.close()
     conn.close()
     print('closed connection to db')
-
